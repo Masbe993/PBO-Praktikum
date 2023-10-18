5f5fc934405b49b7b601e424f2890cd43df6c7f5
Masbe993 <114644735+Masbe993@users.noreply.github.com>
2023-10-18 14:39:15 -0200

    Add files via upload

---
+++ tugas2/Matematika.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas2;
+
+public class Matematika {
+    
+    int pertambahan (int a ,int b){
+        int hasil = a+b;
+        return hasil ;   
+    }
+    
+     int pengurangan (int a,int b){
+        int hasil = a-b;
+        return hasil ;  
+    }
+     
+      int pembagian (int a,int b){
+        int hasil = a/b;
+        return hasil ;
+    }
+    
+     int perkalian (int a,int b){
+        int hasil = a*b;
+        return hasil ;
+       
+    }
+    
+}
